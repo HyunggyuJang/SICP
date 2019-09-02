0e9b452afb2911c2645ae201362940fbe9ba7aa3
Hyunggyu Jang <murasakipurplez5@gmail.com>
2019-09-02 19:35:40 +0900

    bug fix using bisection search

--- Exercise/ex2_5.scm
+++ Exercise/ex2_5.scm
@@ -424,16 +424,13 @@
       (cond (proc (apply proc (map contents args)))
             ((have-same-types? type-tags) (raise-exception)) ;error
             ((all-in-tower? type-tags)
-             ;; (drop
-              (apply
-               apply-generic
-               (cons op
-                     (do-until raise-minimum
-                               (lambda (args)
-                                 (have-same-types? (map type-tag args)))
-                               (raise-minimum args))))
-              ;; )
-             )
+             (drop (apply
+                    apply-generic
+                    (cons op
+                          (do-until raise-minimum
+                                    (lambda (args)
+                                      (have-same-types? (map type-tag args)))
+                                    (raise-minimum args))))))
             (else
              (coerce-and-apply op args type-tags raise-exception))))))
 
@@ -473,7 +470,8 @@
   (put 'make 'real exact->inexact)
   (put 'make 'integer inexact->exact)
   ;; we need to change this accordingly to the polynomial division.
-  (put 'div '(integer integer) quotient)
+  ;; (put 'div '(integer integer) quotient)       ;due to this, the greatest-common-divisor doesn't work
+  (put 'div '(integer integer) /)
   (put 'gcd '(integer integer) gcd)
   (put 'div '(real real) /)
   (define subtypes '(real integer))
@@ -653,6 +651,7 @@
 (define mul (make-generic-op 'mul))
 (define div (make-generic-op 'div))
 (define greatest-common-divisor (make-generic-op 'gcd))
+; (define rem (make-generic-op 'rem))
 
 (define (real-part z) (apply-generic 'real-part z))
 (define (imag-part z) (apply-generic 'imag-part z))
@@ -929,14 +928,15 @@
            (let ((t1 (first-term L1)) (t2 (first-term L2)))
              (cond ((< (order t1) (order t2))
                     (adjoin-term
-                     t2
+                     (make-term (order t2)
+                                (neg (coeff t2)))
                      (sub-terms L1
                                 (rest-terms L2))))
                    ((> (order t1) (order t2))
                     (adjoin-term
                      t1
-                     (sub-terms L2
-                                (rest-terms L1))))
+                     (sub-terms (rest-terms L1)
+                                L2)))
                    (else
                     (adjoin-term
                      (make-term (order t1)
@@ -999,6 +999,9 @@
   (define div-poly
     (arith-poly (lambda (p1 p2) (car (div-terms p1 p2))) "DIV-POLY"))
 
+  ; (define rem-poly
+  ;   (arith-poly remainder-terms "REM-POLY"))
+
   (define gcd-poly
     (arith-poly gcd-terms "GCD-POLY"))
 
@@ -1035,7 +1038,7 @@
 
   (define (->appropriate-rep L)
     (let ((ratio 0.3)) ;determine the ratio at which change the representation of term list
-      (cond ((empty-termlist? L) L)
+      (cond ((or (empty-termlist? L) (= (order (first-term L)) 0)) L)
             ((< (/ (number-of-terms L) (order (first-term L)))
                 ratio)
              (->sparse L))
@@ -1071,6 +1074,8 @@
        (lambda (p1 p2) (tag (sub-poly p1 p2))))
   (put 'div '(polynomial polynomial)
        (lambda (p1 p2) (tag (div-poly p1 p2))))
+  ; (put 'rem '(polynomial polynomial)
+  ;      (lambda (p1 p2) (tag (rem-poly p1 p2))))
   (put 'gcd '(polynomial polynomial)
        (lambda (p1 p2) (tag (gcd-poly p1 p2))))
   (put 'make 'polynomial
@@ -1099,7 +1104,7 @@
   (install-raise)
   (install-project)
   (install-polynomial-package)
-  (install-multivariate-polynomial-package)
+  ;; (install-multivariate-polynomial-package)
   'doneAll)
 ;; Exercise 2.88
 (define (neg x) (apply-generic 'neg x))
@@ -1461,6 +1466,7 @@
   ;; (mul test test1)
   )
 
-; (define p1 (make-polynomial 'x '(sparse (term 2 1) (term 0 1))))
-; (define p2 (make-polynomial 'x '(sparse (term 3 1) (term 0 1))))
-; (define rf (make-rational p2 p1))
+;; (define p1 (make-polynomial 'x '(sparse (term 4 1) (term 3 -1) (term 2 -2) (term 1 2))))
+;; (define p2 (make-polynomial 'x '(sparse (term 3 1) (term 1 -1))))
+;; (greatest-common-divisor p1 p2)
+; (div '(polynomial x sparse (term 5 1) (term 0 -1)) '(polynomial x sparse (term 2 1) (term 0 -1)))
