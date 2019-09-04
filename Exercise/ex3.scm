f419de9f712a4678fcd86b9b07c74c8904ba9d05
Hyunggyu Jang <murasakipurplez5@gmail.com>
2019-09-04 17:03:13 +0900

    Until chapter 3.2.3

--- Exercise/ex3.scm
+++ Exercise/ex3.scm
@@ -32,8 +32,7 @@
 ;; ;Value: 1
 
 ;; Exercise 3.3
-
-(define (make-account balance password)
+(define (make-account0 balance password)
   (define (withdraw amount)
     (if (>= balance amount)
         (begin (set! balance (- balance amount))
@@ -49,7 +48,7 @@
             (set! n-incorrect 0)        ;reset the counter
             (cond ((eq? m 'withdraw) withdraw)
                   ((eq? m 'deposit) deposit)
-                  (else (error "Unknown request -- MAKE-ACCOUNT"
+                  (else (error "Unknown request -- MAKE-ACCOUNT0"
                                m))))
           (lambda (x)
             (set! n-incorrect (1+ n-incorrect))
@@ -60,7 +59,7 @@
 
 
 ;; test code
-;; (define acc (make-account 100 'secret-password))
+;; (define acc (make-account0 100 'secret-password))
 ;; ((acc 'secret-password 'withdraw) 40)
 ;; 60
 ;; ((acc 'some-other-password 'deposit) 50)
@@ -70,7 +69,7 @@
 
 ;; Test code
 ;; ;;; consequetive call case
-;; (define acc (make-account 100 'secret-password))
+;; (define acc (make-account0 100 'secret-password))
 ;; ((acc 'some-other-password 'deposit) 50)
 ;; "Incorrect password"
 ;; ((acc 'some-other-password 'deposit) 50)
@@ -111,3 +110,238 @@
 ;; "Incorrect password"
 ;; ((acc 'some-other-password 'deposit) 50)
 ;; "call-the-cops"
+
+;; Exercise 3.5
+;; top level
+(define (estimate-integral P rect trials)
+  (* (rect 'area)
+     (monte-carlo trials
+                  (lambda () (P (random-in-rect rect))))))
+;; dependency
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+;;; test estimate-integral
+;; (define p1 (make-point -1. -1.))
+;; (define p2 (make-point 1. 1.))
+;; (define r (make-rect p1 p2))
+;; (define (P pt) (<= (+ (square (x-coor pt))
+;;                       (square (y-coor pt)))
+;;                    1))
+;; (estimate-integral P r 100)
+;; it should converge to 3.141592...
+
+;; middle level
+(define (random-in-rect rect)
+  (let ((points (list (bottom-left rect) (top-right rect))))
+    (make-point (apply
+                 random-in-range
+                 (map exact->inexact (map x-coor points)))
+                (apply
+                 random-in-range
+                 (map exact->inexact (map y-coor points))))))
+;; dependency
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+;;; test random-in-rect
+;; (define p1 (make-point 3 4))
+;; (define p2 (make-point 8 7))
+;; (define r (make-rect p1 p2))
+;; (random-in-rect r)
+;; some float point in rect
+
+
+;; low-middle level
+(define (make-rect bl tr)
+  (define area
+    (* (- (x-coor tr) (x-coor bl))
+       (- (y-coor tr) (y-coor bl))))
+  (define (dispatch m)
+    (cond ((eq? m 'top-right) tr)
+          ((eq? m 'bottom-left) bl)
+          ((eq? m 'area) area)
+          (else (error "Undefined request -- MAKE-RECT" m))))
+  dispatch)
+
+(define (top-right rect) (rect 'top-right))
+(define (bottom-left rect) (rect 'bottom-left))
+
+;;; test rect
+;; (define p1 (make-point 3 4))
+;; (define p2 (make-point 8 7))
+;; (define r (make-rect p1 p2))
+;; (r 'area)
+;; 15
+;; (top-right r)
+;; (8 . 7)
+;; (bottom-left r)
+;; (3 . 4)
+;; (r 'unknown-message)
+;; Undefined request -- MAKE-RECT unknown-message
+
+;; lowest level
+(define (make-point x y)
+  (cons x y))
+(define (x-coor pt) (car pt))
+(define (y-coor pt) (cdr pt))
+
+;;; test point
+;; (define a (make-point 5 3))
+;; (x-coor a)
+;; 5
+;; (y-coor a)
+;; 3
+
+;; Exercise 3.6
+;;; dependency
+;;For Section 3.1.2 -- written as suggested in footnote,
+;; though the values of a, b, m may not be very "appropriately chosen"
+(define (rand-update x)
+  (let ((a 27) (b 26) (m 127))
+    (modulo (+ (* a x) b) m)))
+;; *following uses rand-update -- see ch3support.scm
+;; *also must set random-init to some value
+(define random-init 7)			;**not in book**
+
+(define rand
+  (let ((x random-init))
+    (define (dispatch m)
+      (cond ((eq? m 'generate)
+             (set! x (rand-update x))
+             x)
+            ((eq? m 'reset)
+             (lambda (new-x)
+               (set! x new-x)))
+            (else
+             error "Unknown request -- RAND" m)))
+    dispatch))
+
+;;; test rend
+;; (rand 'generate)
+;; <some-random-number>
+;; ((rand 'reset) 5)
+;; (rand 'generate)
+;; <specific-random-number>
+;; (rand 'generate)
+;; <some-other-random-number>
+;; ((rand 'reset) 5)
+;; (rand 'generate)
+;; <specific-random-number>
+
+;; Exercise 3.7
+
+;;; first version
+(define (make-account1 balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((n-incorrect 0)
+        (pw-list (list password)))
+    (define (dispatch pd m)
+      (if (mem? pd pw-list)
+          (begin
+            (set! n-incorrect 0)        ;reset the counter
+            (cond ((eq? m 'withdraw) withdraw)
+                  ((eq? m 'deposit) deposit)
+                  ((eq? m 'joint) (lambda (new-pw)
+                                    (set! pw-list (cons new-pw pw-list))
+                                    dispatch))
+                  (else (error "Unknown request -- MAKE-ACCOUNT1"
+                               m))))
+          (lambda (x)
+            (set! n-incorrect (1+ n-incorrect))
+            (if (>= n-incorrect 7)
+                "call-the-cops"
+                "Incorrect password"))))
+    dispatch))
+
+(define (make-joint acc old-pw new-pw)
+  ((acc old-pw 'joint) new-pw))
+
+;;; test make-joint
+;; (define peter-acc (make-account1 100 'open-sesame))
+;; (define paul-acc (make-joint peter-acc 'open-sesame 'rosebud))
+;; ((paul-acc 'rosebud 'withdraw) 50)
+;; ;; 50
+;; ((peter-acc 'open-sesame 'deposit) 30)
+;; ;; 80
+;; (define opaque (make-joint paul-acc 'open-sesame 'this-should-not-work?))
+
+;; general helper function
+(define (mem? el S)
+  (if (null? S) false
+      (or (eq? el (car S))
+          (mem? el (cdr S)))))
+
+;;; second version
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (make-protected password)
+    (let ((n-incorrect 0))
+      (define (protected-dispatch pd m)
+        (if (eq? pd password)
+            (begin
+              (set! n-incorrect 0)      ;reset the counter
+              (cond ((eq? m 'withdraw) withdraw)
+                    ((eq? m 'deposit) deposit)
+                    ((eq? m 'joint) (lambda (new-pw)
+                                      ((dispatch 'make-protected) new-pw)))
+                    (else (error "Unknown request -- MAKE-PROTECTED"
+                                 m))))
+            (lambda (x)
+              (set! n-incorrect (1+ n-incorrect))
+              (if (>= n-incorrect 7)
+                  "call-the-cops"
+                  "Incorrect password"))))
+      protected-dispatch))
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          ((eq? m 'make-protected) make-protected)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+  dispatch)
+
+(define (make-account2 balance password)
+  (((make-account balance) 'make-protected) password))
+
+;;; test make-joint
+;; (define peter-acc (make-account2 100 'open-sesame))
+;; (define paul-acc (make-joint peter-acc 'open-sesame 'rosebud))
+;; ((paul-acc 'rosebud 'withdraw) 50)
+;; ;; 50
+;; ((peter-acc 'open-sesame 'deposit) 30)
+;; ;; 80
+;; (define opaque (make-joint paul-acc 'open-sesame 'this-should-not-work?))
+
+;; Exercise 3.8
+(define f
+  (let ((x 0))
+    (lambda (n)
+      (let ((temp x))
+        (begin (set! x n)
+               temp)))))
+;; test f
+;; (+ (f 0) (f 1))
+;; (+ (f 1) (f 0))
