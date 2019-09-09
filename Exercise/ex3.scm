8e3a81d7761e5d04d7975959aa04b4214d809c29
Hyunggyu Jang <murasakipurplez5@gmail.com>
2019-09-09 13:56:25 +0900

    Ex 3.31

--- Exercise/ex3.scm
+++ Exercise/ex3.scm
@@ -1139,3 +1139,193 @@
 ;; (insert! 5 'e tbl)
 ;; (insert! -3 'z tbl)
 ;; (lookup -3 tbl)
+
+;; A Simulator for Digital Circuits
+;;; consturct half-adder
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+;; construct full-adder
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+;; primitive function boxes
+;;; inverter
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+;;; and-gate
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+(define (logical-and s1 s2)
+  (cond ((and (= s1 0) (= s2 0)) 0)
+        ((and (= s1 0) (= s2 1)) 0)
+        ((and (= s1 1) (= s2 0)) 0)
+        ((and (= s1 1) (= s2 1)) 1)
+        (else (error "Invalid signals" (cons s1 s2)))))
+;;; or-gate
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+(define (logical-or s1 s2)
+  (cond ((and (= s1 0) (= s2 0)) 0)
+        ((and (= s1 0) (= s2 1)) 1)
+        ((and (= s1 1) (= s2 0)) 1)
+        ((and (= s1 1) (= s2 1)) 1)
+        (else (error "Invalid signals" (cons s1 s2)))))
+
+;; Exercise 3.29
+(define (or-gate2 a1 a2 output)
+  (let ((not-a1 (make-wire))
+        (not-a2 (make-wire))
+        (not-or (make-wire)))
+    (inverter a1 not-a1)
+    (inverter a2 not-a2)
+    (and-gate not-a1 not-a2 not-or)
+    (inverter not-or output)
+    'ok))
+
+;; Exercise 3.30
+(define (ripple-carry-adder aAs aBs aSs C)
+  (define (connect-and-return-carry Ak Bk Ck Sk)
+    (let ((C_k-1 (make-wire)))
+      (full-adder Ak Bk Ck Sk C_k-1)
+      C_k-1))
+  (define (connect-recursive As Bs Ss)
+    (cond ((and (null? As) (null? Bs) (null? Ss))
+           (make-wire))                          ;C_n
+          ((or (null? As) (null? Bs) (null? Ss)) ;defensive programming
+           (error
+            "arguments do not agree in the number of elements --RIPPLE-CARRY-ADDER"
+            (list aAs aBs aSs)))
+          (else
+           (connect-and-return-carry
+            (car As) (car Bs)
+            (connect-recursive (cdr As) (cdr Bs) (cdr Ss))
+            (car Ss)))))
+  (cond ((or (null? aAs) (null? aBs) (null? aSs))
+         (error "RIPPLE-CARRAY-ADDER cannot do connect with given arguemens"
+                (list aAs aBs aSs)))
+        (else (full-adder (car aAs)
+                          (car aBs)
+                          (connect-recursive ;C_1
+                           (cdr aAs)
+                           (cdr aBs)
+                           (cdr aSs))
+                          (car aSs)
+                          C)
+              'ok)))
+
+;; Representing wires
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+    dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+;; implementing after-delay
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+;; initiate simulation
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display "  New-value = ")
+                 (display (get-signal wire)))))
+
+;; sample simulation
+;; ;;; setup
+;; (define the-agenda (make-agenda))
+;; (define inverter-delay 2)
+;; (define and-gate-delay 3)
+;; (define or-gate-delay 5)
+;; (define input-1 (make-wire))
+;; (define input-2 (make-wire))
+;; (define sum (make-wire))
+;; (define carry (make-wire))
+;; (probe 'sum sum)
+;; (probe 'carry carry)
+
+;; ;;; set situation and start
+;; (half-adder input-1 input-2 sum carry)
+;; (set-signal! input-1 1)
+;; (propagate)
+
+;; ;;; second situation and start
+;; (set-signal! input-2 1)
+;; (propagate)
