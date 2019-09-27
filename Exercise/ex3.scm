56ffc446f165fa1756bbd06088afb431abd2547a
Hyunggyu Jang <murasakipurplez5@gmail.com>
2019-09-27 16:48:03 +0900

    Back to the normal trace of text book -- 3.3.5

--- Exercise/ex3.scm
+++ Exercise/ex3.scm
@@ -1398,3 +1398,41 @@
       (let ((first-seg (first-segment agenda)))
         (set-current-time! agenda (segment-time first-seg)) ;first item access renew the time
         (front-queue (segment-queue first-seg))))) ;from the contract, queue is not empty
+
+;; Section 3.3.5
+(define (adder a1 a2 sum)
+  (define (process-new-value)
+    (cond ((and (has-value? a1) (has-value? a2))
+           (set-value! sum
+                       (+ (get-value a1) (get-value a2))
+                       me))
+          ((and (has-value? a1) (has-value? sum))
+           (set-value! a2
+                       (- (get-value sum) (get-value a1))
+                       me))
+          ((and (has-value? a2) (has-value? sum))
+           (set-value! a1
+                       (- (get-value sum) (get-value a2))
+                       me))))
+  (define (process-forget-value)
+    (forget-value! sum me)
+    (forget-value! a1 me)
+    (forget-value! a2 me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+           (error "Unknown request -- ADDER" request))))
+  (connect a1 me)
+  (connect a2 me)
+  (connect sum me)
+  me)
+
+;; emphasize the aspect of constraint as data
+(define (inform-about-value constraint)
+  (constraint 'I-have-a-value))
+(define (inform-about-no-value constraint)
+  (constraint 'I-lost-my-value))
