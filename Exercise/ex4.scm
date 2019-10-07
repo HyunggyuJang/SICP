f1403b5ee5527201a21584bfc92958bb723f3d2c
Hyunggyu Jang <murasakipurplez5@gmail.com>
2019-10-07 16:48:35 +0900

    Until ex 4.19

--- Exercise/ex4.scm
+++ Exercise/ex4.scm
@@ -1,28 +1,234 @@
 ;; For section 4.1.1 ~ 4.1.4
 (load "ch4-mceval.scm")
+
+;; Exercise 4.11
+;; ;; For extend-environment
+;; (define (make-frame vars vals)
+;;   (let ((bindings
+;;          ;; provided that the length of both arguments match
+;;          (fold-right
+;;           (lambda (var val bindings)
+;;             (cons (list var val) bindings))
+;;           '()
+;;           vars
+;;           vals))
+;;         (tbl (make-table)))
+;;     (set-bindings! tbl bindings)
+;;     tbl))
+
+;; ;; Table ADT
+;; ;; constructor
+;; (define (make-table)
+;;   (list '*table*))
+;; ;; mutator
+;; (define set-bindings! set-cdr!)
+;; ;; selector
+;; (define bindings cdr)
+
+;; ;; For lookup-variable-value
+;; (define (lookup-variable-value var env)
+;;   (let env-loop ((env env))
+;;     (if (eq? env the-empty-environment)
+;;         (error "Unbound variable" var)
+;;         (let ((frame (first-frame env)))
+;;           (cond ((assoc var (bindings frame)) => cadr)
+;;                 (else (env-loop
+;;                        (enclosing-environment env))))))))
+
+;; ;; For set-variable-value!
+;; (define (set-variable-value! var val env)
+;;   (let env-loop ((env env))
+;;     (if (eq? env the-empty-environment)
+;;         (error "Unbound variable -- SET!" var)
+;;         (let ((frame (first-frame env)))
+;;           (cond ((assoc var (bindings frame))
+;;                  => (lambda (b) (set-car! (cdr b) val)))
+;;                 (else (env-loop
+;;                        (enclosing-environment env))))))))
+
+;; ;; For define-variable!
+;; (define (define-variable! var val env)
+;;   (let ((frame (first-frame env)))
+;;     (cond ((assoc var (bindings frame))
+;;                  => (lambda (b) (set-car! (cdr b) val)))
+;;           (else (set-bindings! frame
+;;                                (cons (list var val)
+;;                                      (bindings frame)))))))
+
+;; ;; Test for frame as table
+;; (install-eval-clauses)
+;; ;; test definition
+;; (eval '(define test-def 0) the-global-environment)
+;; ;; ok
+
+;; ;; test assignment in nested frame
+;; (eval '(define (test-assignment) (set! test-def 5))
+;;       the-global-environment)
+;; ;; ok
+
+;; (eval 'test-def the-global-environment)
+;; ;; 0
+
+;; (eval '(test-assignment) the-global-environment)
+;; ;; ok
+
+;; ;; test lookup-variable-value
+;; (eval 'test-def the-global-environment)
+;; ;; 5
+
+;; ;; verify that we using the new data structure for frame
+;; (caar the-global-environment)
+;; ;; *table*
+
+;; Exercise 4.13
+
+;; syntax procedure for make-unbound! with semantic
+(define (install-eval-make-unbound)
+  (define var cadr)
+  (put 'eval 'make-unbound!
+       (lambda (exp env)
+         (unbound-variable! (var exp)
+                            env)
+         'ok))
+  'done)
+
+(define (unbound-variable! var env)
+  (remove-binding-from-frame!
+   var
+   (first-frame env)
+   (lambda ()
+     (error "Unbound variable in the given frame -- MAKE-UNBOUND!" var))))
+
+;; Change the representation of frame
+(define (make-frame variables values)
+  (cons (cons '*variables* variables)
+        (cons '*values* values)))
+
+;; selectors for frame
+(define (frame-variables frame) (cdar frame))
+(define (frame-values frame) (cddr frame))
+
+;; mutators of frame
+(define (add-binding-to-frame! var val frame)
+  (set-cdr! (car frame) (cons var (frame-variables frame)))
+  (set-cdr! (cdr frame) (cons val (frame-values frame))))
+
+(define (remove-binding-from-frame! var frame exception)
+  ((find-var-and-apply-to-bindings
+      var cadr
+      (lambda (h-vars h-vals)
+        (set-cdr! h-vars (cddr h-vars))
+        (set-cdr! h-vals (cddr h-vals)))
+      car cdr (lambda (h-vars) (null? (cdr h-vars))))
+     exception
+     frame))
+
+;; operations on frame
+(define (find-var-and-apply-to-bindings
+         var find-op bindings-op frame-vars frame-vals empty-vars?)
+  (lambda (null-op frame)
+    (let scan ((vars (frame-vars frame))
+               (vals (frame-vals frame)))
+      (cond ((empty-vars? vars)
+             (null-op))
+            ((eq? var (find-op vars))
+             (bindings-op vars vals))
+            (else (scan (cdr vars) (cdr vals)))))))
+
+(define (find-var-and-apply-in-frame var vals-op)
+  (find-var-and-apply-to-bindings
+   var car (lambda (vars vals) (vals-op vals))
+   frame-variables frame-values null?))
+
+;; Test for make-unbound!
+;; (install-eval-make-unbound)
+;; (eval '(make-unbound! test-def) the-global-environment)
+
+;; Exercise 4.12
+;; Here we define new datatype namely FrameOp
+;; for interfacing with environment operations:
+;; FrameOp := Frame, NullOp -> Any
+;; NullOp := void -> Any
+
+;; Var, (Vals -> Any) -> FrameOp
+;; (define (find-var-and-apply-in-frame var vals-op)
+;;   (lambda (null-op frame)
+;;     (let scan ((vars (frame-variables frame))
+;;                (vals (frame-values frame)))
+;;       (cond ((null? vars)
+;;              (null-op))
+;;             ((eq? var (car vars))
+;;              (vals-op vals))
+;;             (else (scan (cdr vars) (cdr vals)))))))
+
+;; FrameOp, void -> Any, Env
+;; -> Any
+(define (traverse-env-using frame-op empty-env-op env)
+  (if (eq? env the-empty-environment)
+      (empty-env-op)
+      (frame-op (lambda () (traverse-env-using frame-op empty-env-op
+                                               (enclosing-environment env)))
+                (first-frame env))))
+
+(define (lookup-variable-value var env)
+  (traverse-env-using
+   (find-var-and-apply-in-frame
+    var
+    (lambda (vals)
+      (let ((val (car vals)))
+        (if (eq? val undef)
+            (error "Unassigned variable" var)
+            val))))
+   (lambda () (error "Unbound variable" var))
+   env))
+
+(define (set-variable-value! var val env)
+  (traverse-env-using
+   (find-var-and-apply-in-frame
+    var
+    (lambda (vals) (set-car! vals val)))
+   (lambda () (error "Unbound variable -- SET!" var))
+   env))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    ((find-var-and-apply-in-frame
+      var
+      (lambda (vals) (set-car! vals val)))
+     (lambda ()
+       (add-binding-to-frame! var val frame))
+     frame)))
+
+
+
+;; initialize the startup environment
 (define primitive-procedures
   (append (list
+           (list '> >)
+           (list '< <)
            (list '- -)
            (list '= =)
            (list '+ +)
-           (list '* *))
-        primitive-procedures))
+           (list '* *)
+           (list '-1+ -1+)
+           (list '1+ 1+))
+          primitive-procedures))
 (define the-global-environment (setup-environment))
 ;; Exercise 4.1
 ;;; Left to Right
-(define (list-of-values exps env)
-  (if (no-operands? exps)
-      '()
-      (let ((first (eval (first-operand exps) env)))
-        (cons first
-              (list-of-values (rest-operands exps) env)))))
+;; (define (list-of-values exps env)
+;;   (if (no-operands? exps)
+;;       '()
+;;       (let ((first (eval (first-operand exps) env)))
+;;         (cons first
+;;               (list-of-values (rest-operands exps) env)))))
 ;;; Right to Left
-(define (list-of-values exps env)
-  (if (no-operands? exps)
-      '()
-      (let ((rest (list-of-values (rest-operands exps) env)))
-        (cons (eval (first-operand exps) env)
-              rest))))
+;; (define (list-of-values exps env)
+;;   (if (no-operands? exps)
+;;       '()
+;;       (let ((rest (list-of-values (rest-operands exps) env)))
+;;         (cons (eval (first-operand exps) env)
+;;               rest))))
 
 ;; Exercise 4.3
 (define (eval exp env)
@@ -315,3 +521,189 @@
 ;;  '(fib 3)
 ;;  the-global-environment)
 ;; ;; 2
+
+;; Exercise 4.9
+;;; Implement while loop
+(define (install-eval-while)
+  (define pred cadr)
+  (define body cddr)
+  (define (eval-while exp env)
+    (let ((bexp (sequence->exp (body exp))))
+      (let loop ()
+        (if (true? (eval (pred exp) env))
+            (begin (eval bexp env)
+                   (loop))
+            'done))))
+  (put 'eval 'while eval-while)
+  'done)
+;; test for while
+;; (eval '(begin
+;;          (define x 5)
+;;          (define sum 0)
+;;          (while (> x 0)
+;;            (set! sum (+ x sum))
+;;            (set! x (-1+ x)))
+;;          sum)
+;;       the-global-environment)
+;; 15
+
+;; Exercise 4.10
+;; (define (eval exp env)
+;;   (cond ((self-evaluating? exp) exp)
+;;         ((variable? exp) (lookup-variable-value exp env))
+;;         ((quoted? exp) (text-of-quotation exp))
+;;         ((assignment? exp) (eval-assignment exp env))
+;;         ((definition? exp) (eval-definition exp env))
+;;         ((if? exp) (eval-if exp env))
+;;         ((lambda? exp)
+;;          (make-procedure (lambda-parameters exp)
+;;                          (lambda-body exp)
+;;                          env))
+;;         ((begin? exp)
+;;          (eval-sequence (begin-actions exp) env))
+;;         ((cond? exp) (eval (cond->if exp) env))
+;;         ((application? exp)
+;;          (apply (eval (operator exp) env)
+;;                 (list-of-values (operands exp) env)))
+;;         (else
+;;          (error "Unknown expression type -- EVAL" exp))))
+
+;; (define (infix-tag-list? exp tag)
+;;   (and (pair? exp)
+;;        (pair? (cdr exp))
+;;        (eq? (cadr exp) tag)))
+
+;;; assignment
+;; detector
+;; (define (assignment? exp)
+;;   (infix-tag-list? exp '<-))
+
+;; selectors
+;; (define (assignment-variable exp) (car exp))
+;; (define (assignment-value exp) (caddr exp))
+
+;;; definition
+;; detector
+;; (define (definition? exp)
+;;   (infix-tag-list? exp '=))
+
+;; selectors
+;; (define (definition-variable exp)
+;;   (if (symbol? (car exp))
+;;       (car exp)
+;;       (caar exp)))
+;; (define (definition-value exp)
+;;   (if (symbol? (car exp))
+;;       (caddr exp)
+;;       (make-lambda (cdar exp)
+;;                    (cddr exp))))
+
+;; ;; Test infix defintion
+;; (eval '(begin (x = 2)
+;;               x)
+;;       the-global-environment)
+;; ;; 2
+;; ;; procedure definition
+;; (eval
+;;  '((factorial n) = (if (= n 1)
+;;                        1
+;;                        (* n (factorial (-1+ n)))))
+;;  the-global-environment)
+;; ;; ok
+;; (eval '(factorial 3) the-global-environment)
+;; ;; 6
+
+
+;; ;; test infix assignment
+;; (eval '(begin (x <- 5)
+;;               x)
+;;       the-global-environment)
+;; ;; 5
+
+;; Exercise 4.16
+;;; b.
+(define (scan-out-defines proc-body)
+  (let ((extrated-exps
+         ;; List<Exp> (proc-body) -> List<Exp> x List<Exp>
+         ;; We implement this a little bit complicate procedure since
+         ;; we want to examine possible user error --
+         ;; defenstive programming.
+         (fold-right
+          (lambda (exp extracted)
+            (if (definition? exp)
+                (cons (cons exp (car extracted)) (cdr extracted))
+                (if (null? (car extracted))
+                    (cons (car extracted) (cons exp (cdr extracted)))
+                    ;; intertwined internal definitions with others
+                    (error "Internal defintions intertwines with others" proc-body))))
+          (cons '() '())
+          proc-body)))
+    (let ((internal-defs (car extrated-exps))
+          (rest-body (cdr extrated-exps)))
+      (let ((vars (map definition-variable internal-defs))
+            (exps (map definition-value internal-defs)))
+        (let ((bindings
+               (map (lambda (var) (list var (list 'quote undef)))
+                    vars))
+              (set-exps
+               (map (lambda (var val)
+                      (make-assignment var val))
+                    vars
+                    exps)))
+          (make-let bindings (append set-exps rest-body)))))))
+
+;; test for scan-out-defines
+;; (scan-out-defines
+;;  '((define u <e1>)
+;;    (define v <e2>)
+;;    <e3>))
+;; should return
+;; (let ((u '*unassigned*)
+;;       (v '*unassigned*))
+;;   (set! u <e1>)
+;;   (set! v <e2>)
+;;   <e3>)
+(pretty-print (scan-out-defines
+               '((define u <e1>)
+                 (define v <e2>)
+                 <e3>)))
+
+;;; c.
+(define (make-procedure parameters body env)
+  (list 'procedure parameters
+        (scan-out-defines body)
+        env))
+
+;; Exercise 4.17
+(define (scan-out-defines2 proc-body)
+  (let ((extrated-exps
+         ;; List<Exp> (proc-body) -> List<Exp> x List<Exp>
+         ;; We implement this a little bit complicate procedure since
+         ;; we want to examine possible user error --
+         ;; defenstive programming.
+         (fold-right
+          (lambda (exp extracted)
+            (if (definition? exp)
+                (cons (cons exp (car extracted)) (cdr extracted))
+                (if (null? (car extracted))
+                    (cons (car extracted) (cons exp (cdr extracted)))
+                    ;; intertwined internal definitions with others
+                    (error "Internal defintions intertwines with others" proc-body))))
+          (cons '() '())
+          proc-body)))
+    (let ((internal-defs (car extrated-exps))
+          (rest-body (cdr extrated-exps)))
+      (let ((vars (map definition-variable internal-defs))
+            (exps (map definition-value internal-defs)))
+        (let ((def-vars
+               (map (lambda (var) (make-definition var (list 'quote undef)))
+                    vars))
+              (set-exps
+               (map (lambda (var val)
+                      (make-assignment var val))
+                    vars
+                    exps)))
+          (append def-vars (append set-exps rest-body)))))))
+
+(define (make-definition var val)
+  (list 'define var val))
