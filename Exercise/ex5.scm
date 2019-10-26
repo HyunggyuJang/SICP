c87f5b257ffc225ec2d3b2bb4c6c7bc148898e2d
Hyunggyu Jang <murasakipurplez5@gmail.com>
2019-10-26 15:07:07 +0900

    Exercise 5.23

--- Exercise/ex5.scm
+++ Exercise/ex5.scm
@@ -1089,3 +1089,19 @@ abs-done
      last-pair-done
      (perform (op set-cdr!) (reg l-p) (reg y))
      )))
+
+;; Exercise 5.23
+(define (let? exp) (tagged-list? exp 'let))
+(define (let-bindings exp) (cadr exp))
+(define (let-body exp) (cddr exp))
+
+(define (let-var binding) (car binding))
+(define (let-val binding) (cadr binding))
+(define (make-combination operator operands) (cons operator operands))
+
+(define (let->combination exp)
+  ;;make-combination defined in earlier exercise
+  (let ((bindings (let-bindings exp)))
+    (make-combination (make-lambda (map let-var bindings)
+                                   (let-body exp))
+                      (map let-val bindings))))
